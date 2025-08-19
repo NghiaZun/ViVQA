1982809d5080d8bf4865b8683bb0ac7eeb64ec24
Dương Trung Nghĩa <dtnghia22@clc.fitus.edu.vn>
2025-08-19 23:50:17 +0700

    Update train.py

--- train.py
+++ train.py
@@ -10,7 +10,7 @@ from model import VQAGenModel
 # === CONFIGURATION ===
 CSV_PATH = '/kaggle/input/vivqa/ViVQA-main/ViVQA-main/train.csv'
 IMAGE_FOLDER = '/kaggle/input/vivqa/drive-download-20220309T020508Z-001/train'
-CHECKPOINT_DIR = '/kaggle/input/checkpoint/transformers/default/1/checkpoints'
+CHECKPOINT_DIR = '/kaggle/input/checkpoint/transformers/default/V1/checkpoints'
 SAVE_DIR = '/kaggle/working/checkpoints'
 os.makedirs(SAVE_DIR, exist_ok=True)
 
